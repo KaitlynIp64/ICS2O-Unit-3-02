a36060798b4f8a7cb14eeaf91a71f896ccd8409e
KaitlynIp64 <99354960+KaitlynIp64@users.noreply.github.com>
2022-03-31 17:12:05 +0000

    added code

--- js/script.js
+++ js/script.js
@@ -2,8 +2,20 @@
 //
 // Created by: Kaitlyn Ip
 // Created on: Mar 2022
-// This file contains xxx
+// This file contains the JS functions for index.html
 
+'use strict'
 /**
- * This function xxx.
+ * This function calculates perimeter of triangle.
  */
+function calculate () {
+  // input
+  const base = parseInt(document.getElementById('base-of-triangle').value)
+  const height = parseInt(document.getElementById('height-of-triangle').value)
+
+  // process
+  const perimeter = base + height + (base ** 2 + height ** 2) ** 0.5
+
+  // output
+  document.getElementById('area').innerHTML = 'Perimeter is: ' + perimeter.toFixed(2) + ' cm'
+}
