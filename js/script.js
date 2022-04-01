ddf20254ecff77242a9c7b082f036412366e4bb3
KaitlynIp64 <99354960+KaitlynIp64@users.noreply.github.com>
2022-04-01 16:38:39 +0000

    fixed code

--- js/script.js
+++ js/script.js
@@ -12,7 +12,7 @@ function calculate () {
   // input
   const length = parseFloat(document.getElementById("length-of-pyramid").value)
   const width = parseFloat(document.getElementById("width-of-pyramid").value)
-  const height = parseInt(document.getElementById('height-of-pyramid').value)
+  const height = parseFloat(document.getElementById('height-of-pyramid').value)
 
   // process
   const volume = (length * width * height) / 3
